9389702a660bbf3a725db782a055bb67fa4ee21b
alex hong <honguttdc@gmail.com>
2021-11-22 15:42:33 -0800

    initialize

---
+++ main.js
@@ -0,0 +1,14 @@
+/*
+ * Project:
+ * File Name: main.js
+ * Description:
+ *
+ * Created Date:
+ * Author: A Ut Hong
+ *
+ */
+
+const IOhandler = require("./IOhandler"),
+  zipFilePath = `${__dirname}/myfile.zip`,
+  pathUnzipped = `${__dirname}/unzipped`,
+  pathProcessed = `${__dirname}/grayscaled`;
